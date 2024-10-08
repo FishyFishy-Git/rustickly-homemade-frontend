a71770a355705a01144073b0f5e4bc0274c74e24
Tanner <rotton199@gmail.com>
2024-10-07 20:51:50 -0400

    created basic infrastructure for the home/landing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import './App.css';
-//import
 
 function App() {
   return (
     <div className='main-container'>
       <div className='navigation-bar'>
         <img src="assests/Rustickly_2.png" alt="logo for the business Rustickly homemade bakery" />
-        <p id='portfolio'>Portfolio</p>
-        <p id='qoute'>Qoute</p>
+        <p id='portfolio'></p>
+        <p id='qoute'></p>
+        <p id='about-us'></p>
       </div>
+
       <div className='logo-header'>
+
       </div>
-      <div className='content-block'> 
+
+      <div className='content-block'>
+
       </div>
+
       <div className='footer-block'>
         <div className='social-placeholder'>
+
         </div>
       </div>
+
     </div>
   );
 }
