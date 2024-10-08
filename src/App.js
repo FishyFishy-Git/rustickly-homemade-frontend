b91c19bd7cfed1b7875935e2e82e02b2810cfefb
Tanner <rotton199@gmail.com>
2024-10-07 20:41:12 -0400

    fixed file structure

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+//import
+
+function App() {
+  return (
+    <div className='main-container'>
+      <div className='navigation-bar'>
+        <img src="assests/Rustickly_2.png" alt="logo for the business Rustickly homemade bakery" />
+        <p id='portfolio'>Portfolio</p>
+        <p id='qoute'>Qoute</p>
+      </div>
+      <div className='logo-header'>
+      </div>
+      <div className='content-block'> 
+      </div>
+      <div className='footer-block'>
+        <div className='social-placeholder'>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
