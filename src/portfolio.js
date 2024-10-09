eface3529dc5f3df6e51393b923592c0c2532b43
Tanner <rotton199@gmail.com>
2024-10-09 18:21:34 -0400

    updated infrastructure to be multipage

---
+++ src/portfolio.js
@@ -0,0 +1,12 @@
+import './App.css'; 
+import React from "react";
+
+function portfolio() {
+    return(
+        <div>
+            <h1>PLACE HOLDER DIV AND H1</h1>
+        </div>
+    );
+};
+
+export default portfolio;
---
+++ src/qoute.js
@@ -0,0 +1,12 @@
+import './App.css'; 
+import React from "react";
+
+function qoute() {
+    return(
+        <div>
+            <h1>PLACE HOLDER DIV AND H1</h1>
+        </div>
+    );
+};
+
+export default qoute;
