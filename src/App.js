e8257c529e6ad62ec4e601476d06f62545f087b3
John Arena <john.arena1o@gmail.com>
2024-10-09 19:25:49 -0400

    start link tree creation

--- src/App.js
+++ src/App.js
@@ -7,8 +7,19 @@ import {
 } from "react-router-dom"
 
 //import pages
+import landingPage from "./pages/landingPage"
+import about from './pages/about'
+import portfolio from './pages/portfolio';
+import quote from "./pages/qoute"
 
-const router = createBrowserRouter()
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route path="/" element={<landingPage />} />
+      <Route path="/about" element={<about />} />
+    </Route>
+  )
+)
 
 function App() {
   return (
