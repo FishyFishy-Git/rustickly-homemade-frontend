faecad3a27b5b709dd6feaee147cba2787f51edd
Tanner <rotton199@gmail.com>
2024-10-09 18:25:07 -0400

    fixed missing about page

---
+++ src/about.js
@@ -0,0 +1,12 @@
+import './App.css'; 
+import React from "react";
+
+function about() {
+    return(
+        <div>
+            <h1>PLACE HOLDER DIV AND H1</h1>
+        </div>
+    );
+};
+
+export default about;
