ecc627b44867da9608ac8358b36d151a852616c6
John Arena <john.arena1o@gmail.com>
2024-10-16 21:07:14 -0400

    add admin/login content and light styling to page

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,29 @@
-//import css
+import './login.css'
+import { useState } from 'react'
 
 function Login() {
-    return <>login page</>
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    let errorMessage = ""
+    const handleLogin = () => {
+        console.log('login hit');
+        
+    }
+    return (<>
+        <div className="auth-inputs">
+            <div className="username">
+                <label htmlFor="username-input">Username:</label>
+                <input type="text" placeholder="username" id="username-input" onChange={e => setUsername(e.target.value)} />
+            </div>
+            <div className="password">
+                <label htmlFor="password-input">Password:</label>
+                <input type="text" placeholder="password" id="password-input" onChange={e => setPassword(e.target.value)} />
+            </div>
+            <button className="auth-button" onClick={handleLogin}>Log In</button>
+            <p className="authWarning">{errorMessage}</p>
+        </div>
+    </>)
 }
 
 export default Login
