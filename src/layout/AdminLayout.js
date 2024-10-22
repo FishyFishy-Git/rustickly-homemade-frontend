8b40bc4031ccb872058309e0cb7650383fa124d9
John Arena <john.arena1o@gmail.com>
2024-10-21 20:09:04 -0400

    Admin Login page (#6)
    * add admin/login content and light styling to page
    
    * add admin login page with simple styling
    
    * finish endpoint work for logging in
    
    * add admin nav layout and change link tree accordingly
    
    * add more styling to login page
    
    * add admin homepage and redirect successful login to homepage
    
    * add more indepth styling for admin login page

---
+++ src/layout/AdminLayout.js
@@ -0,0 +1,19 @@
+import "./adminLayout.css"
+import { NavLink, Outlet } from "react-router-dom"
+import logo from '../assests/Rustickly_2.png'
+
+function AdminLayout() {
+    return (<>
+        <header>
+            <section >
+                <img src={logo} alt="Rustickly Homemade's logo" id="logo" />
+            </section>            
+            <NavLink>Portfolio</NavLink>
+            <NavLink>About</NavLink>
+            <NavLink id="logout">Logout</NavLink>
+        </header>
+        <Outlet />
+    </>)
+}
+
+export default AdminLayout
