5c6795b3b4dcdeb99de0a9ec4b88fa5ce1afa608
John Arena <john.arena1o@gmail.com>
2024-11-04 21:12:41 -0500

    fix after merge with main

---
+++ src/pages/admin-pages/AdminAbout.js
@@ -0,0 +1,13 @@
+//imports
+import EditorWindow from '../../components/EditorWindow'
+
+function AdminAbout() {
+    return (
+        <div>
+            <h2>About Editor</h2>
+            <EditorWindow />
+        </div>
+    )
+}
+
+export default AdminAbout
