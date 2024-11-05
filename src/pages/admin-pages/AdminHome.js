0e21a1240d76b05d5e27b907dadf9bc98db7b14d
John Arena <john.arena1o@gmail.com>
2024-11-04 21:19:35 -0500

    add placeholder content to the other admin pages and fix some spelling errors throughout the project

---
+++ src/pages/admin-pages/AdminHome.js
@@ -0,0 +1,11 @@
+//imports
+
+function AdminAbout() {
+    return (
+        <div>
+            <h2>Home Editor and landing page from login</h2>
+        </div>
+    )
+}
+
+export default AdminAbout
