c2ee426dacc1e7a2a1ae8213c9f13ac85ba7fa40
John Arena <john.arena1o@gmail.com>
2024-11-08 04:04:27 -0500

    start mobile styling for adminLayout

--- src/layout/AdminLayout.js
+++ src/layout/AdminLayout.js
@@ -1,19 +1,32 @@
 import "./adminLayout.css"
 import { NavLink, Outlet } from "react-router-dom"
 import logo from '../assets/Rustickly_2.png'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars, faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 
 function AdminLayout() {
+    const handleMenu = () => {
+
+    }
+
     return (<>
         <header className="admin-header">
-            <section >
-                <img src={logo} alt="Rustickly Homemade's logo" id="logo" />
-            </section>
+            <FontAwesomeIcon
+                icon={faBars}
+                className={'mobile-icon'}
+                onClick={handleMenu}
+            />
+            <img src={logo} alt="Rustickly Homemade's logo" id="admin-logo" />
             <div className="admin-links">
                 <NavLink to='/admin/' className={'admin-nav'} >Home</NavLink>
                 <NavLink to='/admin/about' className={'admin-nav'} >About</NavLink>
                 <NavLink to='/admin/portfolio' className={'admin-nav'} >Portfolio</NavLink>
-                <NavLink id="logout" className={'admin-nav'} >Logout</NavLink>
             </div>
+            <NavLink id="logout" className={'admin-nav'} >Logout</NavLink>
+            <NavLink>
+                <FontAwesomeIcon icon={faArrowRightFromBracket} className={'mobile-icon'} />
+            </NavLink>
+            
         </header>
         <Outlet />
     </>)
