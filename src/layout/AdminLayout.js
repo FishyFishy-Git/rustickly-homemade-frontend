b6036b0c8a76a9fc570ed5b1050a26fda04c6305
John Arena <john.arena1o@gmail.com>
2024-11-10 14:31:18 -0500

    About and Homepage editors (#12)
    * add admin/login content and light styling to page
    
    * add admin login page with simple styling
    
    * finish endpoint work for logging in
    
    * add admin nav layout and change link tree accordingly
    
    * add more styling to login page
    
    * add admin homepage and redirect successful login to homepage
    
    * add more indepth styling for admin login page
    
    * add admin-pages subfolder to add more clarity in the pages folder
    
    * change password field in admin login to accept enter as an alternative to clicking on the submit button
    
    * fix mistake from my local branch
    
    * change some style to prevent cross-contamination
    
    * revert debugging code
    
    * change admin filenames for more clarity
    
    * remove gray background on adminlogin css file to make way for real styling colors
    
    * add editor to about page in admin
    
    * fix change buttons not being linked to the editor
    
    * change file names and add About.js content that was worked on previously
    
    * fix after merge with main
    
    * fix more things after merge with main
    
    * add placeholder content to the other admin pages and fix some spelling errors throughout the project
    
    * check if page contents match initial load
    
    * start styling about editor page
    
    * continue styling about editor page
    
    * add homepage editor and made editor componenets more page agnostic
    
    * add proper styling to admin nav bar
    
    * change save/cancel button behaviour to work as inteteded

--- src/layout/AdminLayout.js
+++ src/layout/AdminLayout.js
@@ -1,21 +1,22 @@
-import "./adminLayout.css";
-import { NavLink, Outlet } from "react-router-dom";
-import logo from "../assets/Rustickly_2.png";
+import "./adminLayout.css"
+import { NavLink, Outlet } from "react-router-dom"
+import logo from '../assets/Rustickly_2.png'
 
 function AdminLayout() {
-  return (
-    <>
-      <header>
-        <section>
-          <img src={logo} alt="Rustickly Homemade's logo" id="logo" />
-        </section>
-        <NavLink>Portfolio</NavLink>
-        <NavLink>About</NavLink>
-        <NavLink id="logout">Logout</NavLink>
-      </header>
-      <Outlet />
-    </>
-  );
+    return (<>
+        <header className="admin-header">
+            <section >
+                <img src={logo} alt="Rustickly Homemade's logo" id="logo" />
+            </section>
+            <div className="admin-links">
+                <NavLink to='/admin/' className={'admin-nav'} >Home</NavLink>
+                <NavLink to='/admin/about' className={'admin-nav'} >About</NavLink>
+                <NavLink to='/admin/portfolio' className={'admin-nav'} >Portfolio</NavLink>
+                <NavLink id="logout" className={'admin-nav'} >Logout</NavLink>
+            </div>
+        </header>
+        <Outlet />
+    </>)
 }
 
 export default AdminLayout;
